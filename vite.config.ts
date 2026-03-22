b1f5424a8f684ad62c996313063c536673e6caa2
test <test@example.com>
2026-03-22 15:40:22 -0700

    fix(routing): fix typo in base path for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -162,7 +162,7 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  base: process.env.GITHUB_ACTIONS ? "/akanksha-portfolio/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/akankshamattoo_portfolio/" : "/",
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
